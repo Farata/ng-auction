a81fde77d43284dded9c5f1c928e86720ce4d406
Anton Moiseev <a@antonmoiseev.com>
2017-08-29 18:06:09 +0400

    Add link to the ObservableMedia issue

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,8 +10,8 @@ import { Product, ProductService } from '../shared/services';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-  readonly products: Observable<Product[]>;
   readonly columns: Observable<number>;
+  readonly products: Observable<Product[]>;
   readonly breakpointsToColumnsNumber = new Map([
     [ 'xs', 1 ],
     [ 'sm', 2 ],
@@ -25,6 +25,10 @@ export class HomeComponent {
     private productService: ProductService
   ) {
     this.products = this.productService.getAll();
+
+    // If the initial screen size is xs ObservableMedia doesn't emit an event
+    // and grid-list rendering fails. Once the following issue is closed, this
+    // comment can be removed: https://github.com/angular/flex-layout/issues/388
     this.columns = this.media.asObservable()
       .map(mc => this.breakpointsToColumnsNumber.get(mc.mqAlias));
   }
