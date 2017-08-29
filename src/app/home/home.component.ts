7881b5fa05ec2a0335882af17468e83b34bf3322
Anton Moiseev <a@antonmoiseev.com>
2017-08-29 18:02:44 +0400

    Make products grid list responsive

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
+import 'rxjs/add/operator/map';
 import { Component } from '@angular/core';
+import { ObservableMedia } from '@angular/flex-layout';
 import { Observable } from 'rxjs/Observable';
 import { Product, ProductService } from '../shared/services';
 
@@ -9,8 +11,21 @@ import { Product, ProductService } from '../shared/services';
 })
 export class HomeComponent {
   readonly products: Observable<Product[]>;
+  readonly columns: Observable<number>;
+  readonly breakpointsToColumnsNumber = new Map([
+    [ 'xs', 1 ],
+    [ 'sm', 2 ],
+    [ 'md', 3 ],
+    [ 'lg', 4 ],
+    [ 'xl', 5 ],
+  ]);
 
-  constructor(private productService: ProductService) {
+  constructor(
+    private media: ObservableMedia,
+    private productService: ProductService
+  ) {
     this.products = this.productService.getAll();
+    this.columns = this.media.asObservable()
+      .map(mc => this.breakpointsToColumnsNumber.get(mc.mqAlias));
   }
 }
