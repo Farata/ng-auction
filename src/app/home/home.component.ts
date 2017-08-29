a7df591faf78c74b4235643242361d48719e5b6b
Anton Moiseev <a@antonmoiseev.com>
2017-08-29 16:39:00 +0400

    Add product grid list to the home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Product, ProductService } from '../shared/services';
 
 @Component({
   selector: 'nga-home',
   styleUrls: [ './home.component.scss' ],
   templateUrl: './home.component.html'
 })
-export class HomeComponent {}
+export class HomeComponent {
+  readonly products: Observable<Product[]>;
+
+  constructor(private productService: ProductService) {
+    this.products = this.productService.getAll();
+  }
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MdGridListModule } from '@angular/material';
 import { HomeComponent } from './home.component';
 
 @NgModule({
   imports: [
+    CommonModule,
     RouterModule.forChild([
       { path: '', component: HomeComponent }
-    ])
+    ]),
+    FlexLayoutModule,
+    MdGridListModule
   ],
   declarations: [
     HomeComponent
