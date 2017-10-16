759605535c2045480d2de5585457bb5eeace85dc
Anton Moiseev <a@antonmoiseev.com>
2017-10-17 01:16:49 +0400

    Switch from MatTabGroup to MatTabNavBar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,10 @@
 import 'rxjs/add/operator/combineLatest';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/first';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/share';
-import 'rxjs/add/operator/skip';
 import 'rxjs/add/operator/switchMap';
 
-import { AfterViewInit, ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ObservableMedia } from '@angular/flex-layout';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { MatTabChangeEvent, MatTabGroup } from '@angular/material/tabs';
 import { Observable } from 'rxjs/Observable';
 import {
@@ -24,7 +20,7 @@ import {
   templateUrl: './home.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomeComponent implements AfterViewInit {
+export class HomeComponent {
   readonly categories$: Observable<string[]>;
   readonly columns$: Observable<number>;
   readonly products$: Observable<Product[]>;
@@ -36,23 +32,18 @@ export class HomeComponent implements AfterViewInit {
     [ 'xl', 5 ],
   ]);
 
-  @ViewChild(MatTabGroup) matTabGroup: MatTabGroup;
-
   constructor(
     private media: ObservableMedia,
     private productService: ProductService,
     private searchService: SearchService,
-    private route: ActivatedRoute,
-    private router: Router
+    private route: ActivatedRoute
   ) {
     this.categories$ = this.productService.getAllCategories()
-      .map(categories => ['all', 'featured', ...categories])
-      .share(); // To avoid multiple HTTP requests to the products.json file.
+      .map(categories => ['all', ...categories]);
 
     this.products$ = this.route.params
       .switchMap(({ category }) => this.getCategory(category))
       .combineLatest(this.searchService.params)
-      .debounceTime(10) // To prevent sending two HTTP requests when category and search params change nearly at the same time.
       .map(([ products, params ]) => this.filterProducts(products, params));
 
     // If the initial screen size is xs ObservableMedia doesn't emit an event
@@ -62,31 +53,10 @@ export class HomeComponent implements AfterViewInit {
       .map(mc => <number>this.breakpointsToColumnsNumber.get(mc.mqAlias));
   }
 
-  ngAfterViewInit() {
-    // Sets initial selection for the tab bar.
-    const category = this.route.snapshot.params['category'];
-    this.categories$.first().subscribe(categories => {
-      this.matTabGroup.selectedIndex = categories.indexOf(category);
-      this.matTabGroup.selectedTabChange
-        .skip(1) // Data for the initially selected category is already loaded. No need to trigger it one more time.
-        .subscribe(this.onCategoryChange.bind(this));
-    });
-  }
-
-  onCategoryChange(event: MatTabChangeEvent): void {
-    // Updates category parameter for the current route,
-    // which in turn triggers fetching corresponding products.
-    const category = event.tab.textLabel.toLowerCase();
-    this.router.navigate([ category ], { relativeTo: this.route.parent });
-    this.searchService.params.next({}); // Reset search params.
-  }
-
   private getCategory(category: string): Observable<Product[]> {
-    switch (category.toLowerCase()) {
-      case 'all':      return this.productService.getAll();
-      case 'featured': return this.productService.getFeatured();
-      default:         return this.productService.getByCategory(category.toLowerCase());
-    }
+    return category.toLowerCase() === 'all'
+      ? this.productService.getAll()
+      : this.productService.getByCategory(category.toLowerCase());
   }
 
   private filterProducts(products: Product[], params: SearchParams): Product[] {
--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -3,11 +3,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
-interface Store {
-  featured: number[],
-  products: Product[]
-}
-
 export interface Product {
   id: number;
   title: string;
@@ -22,28 +17,22 @@ export class ProductService {
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<Product[]> {
-    return this.http.get<Store>('/data/products.json')
-      .map(data => data.products);
+    return this.http.get<Product[]>('/data/products.json');
   }
 
   getById(productId: number): Observable<Product> {
-    return this.http.get<Store>('/data/products.json')
-      .map(({ products }) => <Product>products.find(p => p.id === productId));
+    return this.http.get<Product[]>('/data/products.json')
+      .map(products => products.find(p => p.id === productId));
   }
 
   getByCategory(category: string): Observable<Product[]> {
-    return this.http.get<Store>('/data/products.json')
-      .map(({ products }) => products.filter(p => p.categories.includes(category)));
+    return this.http.get<Product[]>('/data/products.json')
+      .map(products => products.filter(p => p.categories.includes(category)));
   }
 
   getAllCategories(): Observable<string[]> {
-    return this.http.get<Store>('/data/products.json')
-      .map(({ products }) => products.reduce((all, product) => all.concat(product.categories), <string[]>[]))
+    return this.http.get<Product[]>('/data/products.json')
+      .map(products => products.reduce((all, product) => all.concat(product.categories), []))
       .map(categories => Array.from(new Set(categories)));
-  }
-
-  getFeatured(): Observable<Product[]> {
-    return this.http.get<Store>('/data/products.json')
-      .map(data => data.products.filter(p => data.featured.includes(p.id)));
   }
 }
